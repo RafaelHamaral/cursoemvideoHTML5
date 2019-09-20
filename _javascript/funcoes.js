ff32f536cc10123be63e4f6468add577363f2865
rafael holanda <rafael_holandamaral@hotmail.com>
2019-09-19 21:53:25 -0300

    projeto-glass-html

---
+++ _javascript/funcoes.js
@@ -0,0 +1,11 @@
+function mudaFoto(foto) {
+    document.getElementById("icone").src = foto;
+
+}   
+/* {} - codigo () - codigo
+
+- getElementById: chame pelo ID
+- icone: id da imagem do oculos que é oque eu quero alterar
+- É uma boa pratica separar os codigos de acordo com o tipo de linguagem
+
+*/ 
